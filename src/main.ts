1d2eee0af3e54383247383bba8a4df8568652997
tamiroh <tamiroh71@gmail.com>
2023-03-21 18:07:15 +0900

    Rename SampleSettingTab to SettingTab

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Editor, Notice, Plugin } from "obsidian";
-import { SampleSettingTab } from "./sampleSettingTab";
+import { SettingTab } from "./settingTab";
 import { Settings } from "./types/Settings";
 import { DEFAULT_SETTINGS } from "./constants";
 
@@ -13,7 +13,7 @@ export default class MyPlugin extends Plugin {
 		});
 		this.addStatusBarItem().setText("Status Bar Text");
 		this.addCommands();
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	addCommands() {
