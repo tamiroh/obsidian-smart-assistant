d0cf24c33fed66b3cfbf36d2fdb0c9aa44c7a3f0
tamiroh <tamiroh71@gmail.com>
2023-03-21 18:06:32 +0900

    Remove sampleModal.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { Editor, MarkdownView, Notice, Plugin } from "obsidian";
+import { Editor, Notice, Plugin } from "obsidian";
 import { SampleSettingTab } from "./sampleSettingTab";
-import { SampleModal } from "./sampleModal";
 import { Settings } from "./types/Settings";
 import { DEFAULT_SETTINGS } from "./constants";
 
@@ -19,35 +18,11 @@ export default class MyPlugin extends Plugin {
 
 	addCommands() {
 		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => new SampleModal(this.app).open(),
-		});
-		this.addCommand({
 			id: "sample-editor-command",
 			name: "Sample editor command",
 			editorCallback: (editor: Editor) => {
 				new Notice(editor.getSelection());
 				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean): boolean | void => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
 			},
 		});
 	}
